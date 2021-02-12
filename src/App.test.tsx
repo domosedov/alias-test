520687e1ce25f688a86490eddd5b4adb64c762dc
Aleksandr Grigorii <domosedov.dev@gmail.com>
2021-02-12 18:22:48 +0300

    feat: setup aliases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { Todos } from "@components/Todos";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("render todos", () => {
+  render(<Todos />);
+
+  screen.debug();
 });
---
+++ src/components/Todos/Todos.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const Todos: FC = () => {
+  return (
+    <div>
+      <h1>Todos</h1>
+    </div>
+  );
+};
